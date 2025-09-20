d8e76f15653afb2a37f578c0037519b805b6033c
fareedmunawar-creator <fareedmunawar@aptechsite.net>
2025-09-20 16:53:11 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\QuickCookController;
 use App\Http\Controllers\Admin\AdminAnalyticsController;
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin/dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
