9d8bb0fbf41c22c0a0a80196264291fa6195cc78
fareedmunawar22 <fareedmunawar00@gmail.com>
2025-09-29 00:00:27 +0500

    push

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     protected $fillable = ['user_id','category_id','title','description','image_path','cooking_time','servings','instructions','difficulty'];
-
+    
     public function getStepsAttribute() {
         return $this->instructions ? explode("\n", $this->instructions) : [];
     }
-    
+
     public function user() {
         return $this->belongsTo(User::class);
     }
